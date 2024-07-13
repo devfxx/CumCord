4efaf80a1caa43696b041f5c17284ed68dd3d08a
devfxx <fxx@kty.lol>
2024-07-13 06:11:02 +0300

    fix: Update git remote URL to use git.kty.lol instead of github.com, other patches

--- src/main/updater/http.ts
+++ src/main/updater/http.ts
@@ -84,7 +84,7 @@ async function applyUpdates() {
     return true;
 }
 
-ipcMain.handle(IpcEvents.GET_REPO, serializeErrors(() => `https://github.com/${gitRemote}`));
+ipcMain.handle(IpcEvents.GET_REPO, serializeErrors(() => `https://git.kty.lol/${gitRemote}`));
 ipcMain.handle(IpcEvents.GET_UPDATES, serializeErrors(calculateGitChanges));
 ipcMain.handle(IpcEvents.UPDATE, serializeErrors(fetchUpdates));
 ipcMain.handle(IpcEvents.BUILD, serializeErrors(applyUpdates));
