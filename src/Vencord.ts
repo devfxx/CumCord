f0dcb221b599f38d3b1962c32d6deb843fa73eab
devfxx <fxx@kty.lol>
2024-08-03 12:57:06 +0300

    chore(Vencord): Notification titles. Vencord to CumCord

--- src/Vencord.ts
+++ src/Vencord.ts
@@ -100,7 +100,7 @@ async function init() {
                 await update();
                 if (Settings.autoUpdateNotification)
                     setTimeout(() => showNotification({
-                        title: "Vencord has been updated!",
+                        title: "CumCord has been updated!",
                         body: "Click here to restart",
                         permanent: true,
                         noPersist: true,
@@ -110,7 +110,7 @@ async function init() {
             }
 
             setTimeout(() => showNotification({
-                title: "A Vencord update is available!",
+                title: "A CumCord update is available!",
                 body: "Click here to view the update",
                 permanent: true,
                 noPersist: true,
@@ -128,7 +128,7 @@ async function init() {
                 "Webpack has finished initialising, but some patches haven't been applied yet.",
                 "This might be expected since some Modules are lazy loaded, but please verify",
                 "that all plugins are working as intended.",
-                "You are seeing this warning because this is a Development build of Vencord.",
+                "You are seeing this warning because this is a Development build of CumCord.",
                 "\nThe following patches have not been applied:",
                 "\n\n" + pendingPatches.map(p => `${p.plugin}: ${p.find}`).join("\n")
             );
