5ede5a5b9ce55688d0fbc1a55a2033ec131223ca
devfxx <fxx@kty.lol>
2024-11-15 00:59:12 +0200

    refactor(llm): Update LLM plugin utils.ts

--- src/plugins/llm/utils.ts
+++ src/plugins/llm/utils.ts
@@ -1,14 +1,15 @@
 /* eslint-disable simple-header/header */
 
 import { classNameFactory } from "@api/Styles";
+import { settings } from "./settings";
 
 export const cl = classNameFactory("vc-llm-");
 
-export async function Summarize(text: string) {
+export async function Respond(text: string) {
     try {
-        const prompt = "Summarize this text, what they meant, and what they said in hindsight.";
+        const prompt = settings.store.responsePrompt;
         const requestBody = {
-            text: `${prompt}\n\n${text}`,
+            text: `${prompt}\nMessage: ${text}`,
         };
 
         const response = await fetch("https://llm.kty.lol/", {
@@ -16,9 +17,35 @@ export async function Summarize(text: string) {
             body: JSON.stringify(requestBody)
         }).then(res => res.json());
 
-        return response.data;
+        return trimResponse(response.data);
     } catch (e) {
         console.error(e);
-        return "An error occurred while summarizing the text.";
+        return "An error occurred while responding to the message.";
+    }
+}
+
+function trimResponse(response: string) {
+    let resp = response.trim();
+    if (resp.endsWith(".")) resp = resp.slice(0, -1);
+    if (resp.startsWith("Message: ")) resp = resp.slice(7);
+    return resp;
+}
+
+export async function Rewrite(text: string) {
+    try {
+        const prompt = settings.store.rewritePrompt;
+        const requestBody = {
+            text: `${prompt}\nMessage: ${text}`,
+        };
+
+        const response = await fetch("https://llm.kty.lol/", {
+            method: "POST",
+            body: JSON.stringify(requestBody)
+        }).then(res => res.json());
+
+        return trimResponse(response.data);
+    } catch (e) {
+        console.error(e);
+        return "An error occurred while rewriting the message.";
     }
 }
